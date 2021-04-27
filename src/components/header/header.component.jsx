cf015241e26d69a20521e5e80658eb67a3377545
GreenPolka <53532540+GreenPolka@users.noreply.github.com>
2021-04-27 15:23:45 -0400

    added the header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../asset/crown.svg'; // this will importing the svg file as a ReactComponent and naming it Logo
+import './header.styles.scss';
+import { findRenderedComponentWithType } from "react-dom/test-utils";
+
+// making this a functional component
+const Header = () => (
+  <div className="header">
+    <Link className='logo-container' to="/">
+        <Logo className='logo'/>
+    </Link>
+    <div className='options'>
+    <Link className='option' to="/shop">
+     SHOP
+    </Link>
+    <Link className='option' to="/shop">
+     CONTACT
+    </Link>
+    </div>
+  </div>
+)
+export default Header;
