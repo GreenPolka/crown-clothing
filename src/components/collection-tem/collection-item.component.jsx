4cdd4c267a9684abda129a147b33fc06221f56b5
GreenPolka <53532540+GreenPolka@users.noreply.github.com>
2021-05-05 00:04:59 -0400

    updated shop data, cart is now able to add items

--- src/components/collection-tem/collection-item.component.jsx
+++ src/components/collection-tem/collection-item.component.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React from 'react';
+import {connect} from 'react-redux';
+import CustomButton from '../custom-button/custom-button.component';
+import {addItem} from '../../redux/cart/cart.actions'
+
 import "./collection-item.styles.scss";
 // we made function because we dont need any state, but we need certain values to be passed in.
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+  const {name, price, imageUrl} = item;
+  return (
   <div className="collection-item">
     <div
       className="image"
@@ -14,8 +20,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
           <span className='name'>{name}</span>
           <span className='price'>{price}</span>
       </div>
-
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     
   </div>
-)
-export default CollectionItem;
+)}
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+export default connect(null, mapDispatchToProps)(CollectionItem);
